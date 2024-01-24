efede154f54e7c980a9d85288764a42fc2136ffb
lroux07 <lroux@edenschool.fr>
2024-01-24 11:09:04 +0100

    Focus input

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 import './style.css';
+
+let body = document.querySelector("body");
+let input = document.querySelectorAll('input');
+
+let inputName = document.querySelector("#name");
+let inputEmail = document.querySelector("#email");
+
+function focusName() {
+    inputName.style.borderBottom = "2px solid red";
+
+    body.addEventListener("click", ()=> {
+        inputName.style.borderBottom = "2px solid #9d9d9d";
+        inputEmail.style.borderBottom = "2px solid #9d9d9d";
+    });
+}
+
+function focusEmail() {
+    inputEmail.style.borderBottom = "2px solid red";
+
+    body.addEventListener("click", ()=> {
+        inputEmail.style.borderBottom = "2px solid #9d9d9d";
+    });
+}
+
+inputName.addEventListener("input", focusName);
+inputEmail.addEventListener("input", focusEmail);
