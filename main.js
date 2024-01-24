772574da8dc2fbe8225cef4fcd2a5a8fb83fc5d8
lroux07 <lroux@edenschool.fr>
2024-01-24 12:52:04 +0100

    Footer

--- main.js
+++ main.js
@@ -1,27 +1,48 @@
 import './style.css';
 
 let body = document.querySelector("body");
-let input = document.querySelectorAll('input');
-
 let inputName = document.querySelector("#name");
 let inputEmail = document.querySelector("#email");
+let message = document.querySelector("#message");
+let btn = document.querySelector("#btn");
 
 function focusName() {
-    inputName.style.borderBottom = "2px solid red";
-
-    body.addEventListener("click", ()=> {
-        inputName.style.borderBottom = "2px solid #9d9d9d";
-        inputEmail.style.borderBottom = "2px solid #9d9d9d";
-    });
+    if(inputName.value.length >= 1) {
+        inputName.style.borderBottom = "2px solid #4EE1A0";
+    } if(inputName.value == "") {
+        inputName.style.borderBottom = "2px solid white";
+    }
 }
 
 function focusEmail() {
-    inputEmail.style.borderBottom = "2px solid red";
-
-    body.addEventListener("click", ()=> {
-        inputEmail.style.borderBottom = "2px solid #9d9d9d";
-    });
+    if(inputEmail.value.length >= 1) {
+        inputEmail.style.borderBottom = "2px solid #4EE1A0";
+    } if(inputEmail.value == "") {
+        inputEmail.style.borderBottom = "2px solid white";
+    }
 }
 
-inputName.addEventListener("input", focusName);
+function btnClick() {
+    // if(inputName.value == "" || inputEmail.value == "" && inputName.value.length < 5 || inputEmail.value.length < 5) {
+    //     inputName.style.borderBottom = "2px solid red";
+    //     inputEmail.style.borderBottom = "2px solid red";
+    //     btn.disabled = true;
+    // } else if(inputName.value.length >= 5 || inputEmail.value.length >= 5) {
+    //     btn.disabled = false;
+    //     inputName.style.borderBottom = "2px solid #4EE1A0";
+    //     inputEmail.style.borderBottom = "2px solid #4EE1A0";
+    // }
+    
+    if(inputEmail.value.length == "@" && inputEmail.value == ".fr" || inputEmail.value == ".com") {
+        inputEmail.style.borderBottom = "2px solid #4EE1A0";
+        btn.disabled = false;
+    } else {
+        inputEmail.style.borderBottom = "2px solid red";
+        btn.disabled = true;
+    }
+}
+
+btn.addEventListener("click", btnClick);
+
+inputName.addEventListener("keyup", focusName);
 inputEmail.addEventListener("input", focusEmail);
